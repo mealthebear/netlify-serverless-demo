f809e1391c6e2f780f0737c816f1ee7563478bf9
mealthebear <mealthebear@gmail.com>
2020-07-14 11:04:11 -0700

    Included more test data

--- func/cluster.js
+++ func/cluster.js
@@ -1,31 +1,238 @@
 // /.netlify/functions/cluster
 
-let imageData = [
-  {
-    filename: null,
-    description: "da bear",
-    url: "https://image.shutterstock.com/image-photo/sleeping-bear-600w-578281798.jpg",
-    tags: "bear bears",
-    lat: "33.6240655",
-    lon: "-117.734509"
+let imageData = {
+  "clusters": {
+    "342c7b12c01511ea8d9aacde48001122": {
+      "cluster_id": "342c7b12c01511ea8d9aacde48001122",
+      "cluster_lat": 32.9446,
+      "cluster_lon": -118.329,
+      "level": "1",
+      "type": "location"
+    },
+    "342e0f9ac01511ea8d9aacde48001122": {
+      "cluster_id": "342e0f9ac01511ea8d9aacde48001122",
+      "cluster_lat": 32.9446,
+      "cluster_lon": -118.329,
+      "level": "2",
+      "type": "location"
+    },
+    "342fa846c01511ea8d9aacde48001122": {
+      "cluster_id": "342fa846c01511ea8d9aacde48001122",
+      "cluster_lat": 32.9446,
+      "cluster_lon": -118.329,
+      "level": "3",
+      "type": "location"
+    },
+    "34310736c01511ea8d9aacde48001122": {
+      "cluster_id": "34310736c01511ea8d9aacde48001122",
+      "cluster_lat": 32.9446,
+      "cluster_lon": -118.329,
+      "level": "4",
+      "type": "location"
+    },
+    "357287dcc01511ea8d9aacde48001122": {
+      "cluster_id": "357287dcc01511ea8d9aacde48001122",
+      "cluster_lat": 32.9663,
+      "cluster_lon": -117.265,
+      "level": "1",
+      "type": "location"
+    }
   },
-  {
-    filename: null,
-    description: "cool cat",
-    url: "https://data.whicdn.com/images/339215611/original.jpg",
-    tags: "cool cat coolcat",
-    lat: "33.623858",
-    lon: "-117.735394"
-  },
-  {
-    filename: null,
-    description: "senior software engineer",
-    url: "https://www.malvernelibrary.org/wp-content/uploads/2012/06/Dog-Coding.jpg",
-    tags: "dog coding",
-    lat: "33.623508",
-    lon: "-117.734271"
-  }
-];
+  "images": [
+    {
+      "filename": null,
+      "title": "DSC_2031",
+      "license": "0",
+      "owner": "13903650@N08",
+      "views": 32,
+      "description": "",
+      "tags": "ensenadacruise2010",
+      "date_taken": "2010-05-20T10:16:13.000Z",
+      "date_upload": "2010-05-21T16:32:38.000Z",
+      "lat": 32.9446,
+      "lon": -118.329,
+      "height": 2848,
+      "width": 4288,
+      "size": null,
+      "privacy": null,
+      "url": "https://live.staticflickr.com/4064/4627244344_72ee6dd671_o.jpg",
+      "url_small": "https://live.staticflickr.com/4064/4627244344_f81ae006c4.jpg",
+      "s3": null,
+      "external_id": 4627244344,
+      "last_updated": "2010-08-18T04:19:55.000Z",
+      "rank": 111,
+      "weather": "sky is clear",
+      "clusters": [
+        "342c7b12c01511ea8d9aacde48001122",
+        "342e0f9ac01511ea8d9aacde48001122",
+        "342fa846c01511ea8d9aacde48001122",
+        "34310736c01511ea8d9aacde48001122"
+      ]
+    },
+    {
+      "filename": null,
+      "title": "NCTC 2101-3002 (Coaster) Del Mar California 19.08.2019",
+      "license": "0",
+      "owner": "77599141@N03",
+      "views": 1379,
+      "description": "San Diego-Oceanside Coaster service 657 running along the spectacular Pacific Coast at Del Mar California.\nMany thanks to Andy Goodson for the location gen and transport.\nThis is a stitch of 3 frames.",
+      "tags": "",
+      "date_taken": "2019-08-21T21:27:51.000Z",
+      "date_upload": "2019-08-21T20:31:21.000Z",
+      "lat": 32.9497,
+      "lon": -117.265,
+      "height": 1856,
+      "width": 4000,
+      "size": null,
+      "privacy": null,
+      "url": "https://live.staticflickr.com/65535/48594836732_7f724acda0_o.jpg",
+      "url_small": "https://live.staticflickr.com/65535/48594836732_b85ec11585.jpg",
+      "s3": null,
+      "external_id": 48594836732,
+      "last_updated": "2020-04-08T15:38:10.000Z",
+      "rank": 611,
+      "weather": "few clouds",
+      "clusters": [
+        "357287dcc01511ea8d9aacde48001122"
+      ]
+    },
+    {
+      "filename": null,
+      "title": "DSC_0283",
+      "license": "0",
+      "owner": "91490091@N02",
+      "views": 2747,
+      "description": "",
+      "tags": "",
+      "date_taken": "2014-03-03T01:25:00.000Z",
+      "date_upload": "2014-03-03T03:45:06.000Z",
+      "lat": 32.9582,
+      "lon": -117.267,
+      "height": 4183,
+      "width": 2484,
+      "size": null,
+      "privacy": null,
+      "url": "https://live.staticflickr.com/7346/12895881305_3da654bbc4_o.jpg",
+      "url_small": "https://live.staticflickr.com/7346/12895881305_de836c729b.jpg",
+      "s3": null,
+      "external_id": 12895881305,
+      "last_updated": "2015-08-27T00:03:08.000Z",
+      "rank": 3679,
+      "weather": "broken clouds",
+      "clusters": [
+        "357287dcc01511ea8d9aacde48001122"
+      ]
+    },
+    {
+      "filename": null,
+      "title": "Red-shouldered Hawk",
+      "license": "0",
+      "owner": "7243127@N04",
+      "views": 1146,
+      "description": "",
+      "tags": "bird california nature raptor redshoulderedhawk sandiego wildlife",
+      "date_taken": "2013-03-09T12:31:26.000Z",
+      "date_upload": "2013-03-10T02:20:02.000Z",
+      "lat": 32.9595,
+      "lon": -117.265,
+      "height": 1160,
+      "width": 1739,
+      "size": null,
+      "privacy": null,
+      "url": "https://live.staticflickr.com/8231/8542817131_59638a5829_o.jpg",
+      "url_small": "https://live.staticflickr.com/8231/8542817131_334c674864.jpg",
+      "s3": null,
+      "external_id": 8542817131,
+      "last_updated": "2017-01-24T06:05:48.000Z",
+      "rank": 4230,
+      "weather": "scattered clouds",
+      "clusters": [
+        "357287dcc01511ea8d9aacde48001122"
+      ]
+    },
+    {
+      "filename": null,
+      "title": "Coaster in Del Mar (CA), 19-09-2019 (2)",
+      "license": "0",
+      "owner": "35422840@N03",
+      "views": 1115,
+      "description": "In de middag aan de andere kant van het spoor gestaan, en nu een Coaster onderweg naar Oceanside door Del Mar.\n\nAfternoon spot in Del Mar, here Coaster passing Del Mar towards Oceanside.",
+      "tags": "del mar coaster",
+      "date_taken": "2019-09-19T13:26:55.000Z",
+      "date_upload": "2019-11-09T15:53:46.000Z",
+      "lat": 32.9578,
+      "lon": -117.268,
+      "height": 2373,
+      "width": 3559,
+      "size": null,
+      "privacy": null,
+      "url": "https://live.staticflickr.com/65535/49038370421_709e11abd2_o.jpg",
+      "url_small": "https://live.staticflickr.com/65535/49038370421_2b410d0207.jpg",
+      "s3": null,
+      "external_id": 49038370421,
+      "last_updated": "2020-02-23T20:57:16.000Z",
+      "rank": 678,
+      "weather": "few clouds",
+      "clusters": [
+        "357287dcc01511ea8d9aacde48001122"
+      ]
+    },
+    {
+      "filename": null,
+      "title": "Big Dogs",
+      "license": "1",
+      "owner": "40708728@N04",
+      "views": 10445,
+      "description": "Nothing says summer quite like the county fair.  I only made it to the San Diego county fair once, but the Orange County Fair started this past weekend, so hopefully I'll have another shot at indulging in fried food and carnival games.",
+      "tags": "food color night canon dark amusement fairgrounds hotdog bigdogs sale fair sd corndog late canon5d bluehour hdr delmar foodstand hotdogstand friedfood delmarfair sandiegocountyfair delmarfairgrounds canon5dmarkiii 5d3 5dmarkiii",
+      "date_taken": "2012-06-22T20:05:56.000Z",
+      "date_upload": "2012-07-16T23:53:05.000Z",
+      "lat": 32.9756,
+      "lon": -117.265,
+      "height": 3813,
+      "width": 5705,
+      "size": null,
+      "privacy": null,
+      "url": "https://live.staticflickr.com/8155/7584074712_991a8e5529_o.jpg",
+      "url_small": "https://live.staticflickr.com/8155/7584074712_f20e65162e.jpg",
+      "s3": null,
+      "external_id": 7584074712,
+      "last_updated": "2017-07-30T05:04:00.000Z",
+      "rank": 3533,
+      "weather": "few clouds",
+      "clusters": [
+        "357287dcc01511ea8d9aacde48001122"
+      ]
+    },
+    {
+      "filename": null,
+      "title": "Red Anthuriums In The Light",
+      "license": "0",
+      "owner": "9422878@N08",
+      "views": 8139,
+      "description": "I photographed these beautiful anthuriums last month at the San Diego County Fair in Del Mar, California.\n\nLighting:  My wife was the voice activated light source while holding a Yongnuo manual flash in an 8.6 inch Lastolite soft box at camera left at 10 o'clock,  and just out of the frame.  The flash was triggered with a Yongnuo RF-603N.  I used camera setting that caused the indoors background to go quite dark.\n\nOther plants, flowers, fruit or thingys that I've photographed using strobes can be seen in my Strobe Lit Plant set. For each image in the set, and there are now at least 1400 of them,  I describe how I set up the lighting for that particular shot.  <a href=\"http://www.flickr.com/photos/9422878@N08/sets/72157628079460544/\">www.flickr.com/photos/9422878@N08/sets/72157628079460544/</a>",
+      "tags": "anthuriums flowers fleurs flores sandiegocountyfair delmarfair flowershow floralphotography offcameraflash darkbackground lastoliteezbox softbox yongnuo yongnuorf603n",
+      "date_taken": "2019-06-05T10:26:01.000Z",
+      "date_upload": "2019-07-12T17:38:55.000Z",
+      "lat": 32.9728,
+      "lon": -117.263,
+      "height": 739,
+      "width": 1024,
+      "size": null,
+      "privacy": null,
+      "url": "https://live.staticflickr.com/65535/48266686127_a2f46e3a0d_b.jpg",
+      "url_small": "https://live.staticflickr.com/65535/48266686127_a2f46e3a0d_n.jpg",
+      "s3": null,
+      "external_id": 48266686127,
+      "last_updated": "2019-07-30T01:08:46.000Z",
+      "rank": 681,
+      "weather": "overcast clouds",
+      "clusters": [
+        "357287dcc01511ea8d9aacde48001122"
+      ]
+    }
+  ]
+}
 
 exports.handler = (event, context, callback) => {
 
